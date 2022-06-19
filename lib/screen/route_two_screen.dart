c90d1d8fe67ba1aa3412d7b272f95eeca25dba0e
jyr <jyr@wavers.co.kr>
2022-06-19 11:25:55 +0900

    알고있으면 좋은 Push 메소드들

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
@@ -29,6 +30,44 @@ class RouteTwoScreen extends StatelessWidget {
             );
           },
           child: Text('Push Named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // [HomeScreen(), RouteOneScreen(), RouteTwoScreen(): RouteThreeScreen()로 대체됨, RouteThreeScreen()]
+            // Navigator.of(context).pushReplacement(MaterialPageRoute(
+            //   builder: (_) => RouteThreeScreen(),
+            // ));
+
+            // [HomeScreen(), RouteOneScreen(), RouteTwoScreen(): RouteThreeScreen()로 대체됨, RouteThreeScreen()]
+            Navigator.of(context).pushReplacementNamed(
+              '/three',
+            );
+          },
+          child: Text('Push Replacement'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (_) => RouteThreeScreen(),
+              ),
+              // (route) => false,
+              // false이면 Route Stack에서 제거됨
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('Push And Remove Until'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              '/three',
+              // (route) => false,
+              // false이면 Route Stack에서 제거됨
+              (route) => route.settings.name == '/',
+            );
+          },
+          child: Text('Push Named And Remove Until'),
         ),
       ],
     );
