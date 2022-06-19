d4835485b8c66e3a1aed53c416cc2e4de63b9880
jyr <jyr@wavers.co.kr>
2022-06-19 10:14:43 +0900

    Argument 전달하기

--- lib/screen/route_two_screen.dart
+++ lib/screen/route_two_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Route Two'),
-      ),
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+
+    return MainLayout(
+      title: 'Route Two',
+      children: [
+        Text(
+          'arguments: ${arguments}',
+          textAlign: TextAlign.center,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Pop'),
+        )
+      ],
     );
   }
 }
